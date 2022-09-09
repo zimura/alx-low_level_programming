a37d38577ed2f61d680f5a9507ebf348f7d0831e
zimura <chidimmaibezim0@gmail.com>
2022-09-08 20:55:47 -0700

    Hello. printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
 /**
 * main - print the string in the function
-* Description: using the main function
-* this should "print with proper grammar, but the outcome is a piece of art,"
-* Return: 0
+* description: using the main function
+* this should print "with proper grammar, but the outcome is a piece of art,"
+* return: 0
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
