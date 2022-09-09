3279de43a97c39707a95e82485f3ba069e8295a3
zimura <chidimmaibezim0@gmail.com>
2022-09-08 20:13:13 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* priString - print the string in the put function
+* main - print the string in the put function
 * 
 * Decription: using the priString function
 * this program prints "Programming is like building a multilingual puzzle"
 * Return: 0
 */
 
-int priString(void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
