e1b8a97e0aee06cdd55f684a9aeceb8ee64a0ae1
zimura <chidimmaibezim0@gmail.com>
2022-09-09 23:02:30 -0700

    print lower and upper case of the alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+* main - print alphabetin lower and upper case
+* Description: using main function
+* this program should print "{a...z}{A...z}"
+* Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
