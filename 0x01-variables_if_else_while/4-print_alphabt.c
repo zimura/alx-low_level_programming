b50b4752ae7a84d8c106d81bdfe6483beb03feab
zimura <chidimmaibezim0@gmail.com>
2022-09-11 08:38:00 -0700

    I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print alphabets in lowercase except e and q
+* Description: using main function
+* this program should print "abcdfghijklmnoprstuvwxyx"
+* Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c =< 'z'; c++)
+	{
+		if (c != 'e'; && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
