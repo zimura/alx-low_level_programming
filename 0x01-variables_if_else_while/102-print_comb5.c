b4ac10eb216ea4d1090f5d6728e86055fdbce4b8
zimura <chidimmaibezim0@gmail.com>
2022-09-12 09:55:51 -0700

    three combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - prints outcomes of two digits combination
+* Description: Using the main function
+* this program should print "{00 01,...98 99"}
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 99)
+	{
+		j = i;
+	while (j <= 99)
+	{
+	if (j != i)
+	{
+		putchar((i / 10) + 48);
+		putchar((i % 10) + 48);
+		putchar(' ');
+		putchar((j / 10) + 48);
+		putchar((j % 10) + 48);
+	if (i != 98 && j != 98)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	++j;
+	}
+	++i;
+	}
+	putchar('\n');
+	return (0);
+}
+
