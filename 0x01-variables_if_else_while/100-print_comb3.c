f17ac4c1761813c2eb1755c2cf3f2302934327a7
zimura <chidimmaibezim0@gmail.com>
2022-09-12 01:15:41 -0700

    two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,17 +14,17 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = i+1; j <= 9; j++)
+	for (j = i + 1; j <= 9; j++)
 	{
 		putchar(i + '0');
 		putchar(j + '0');
-	if (i == 9 && j == 9)
+	if (i == 8 && j == 9)
 	{
 		continue;
 	}
 		putchar(',');
 		putchar(' ');
-	}
+	}	
 	}
 	putchar('\n');
 	return (0);
