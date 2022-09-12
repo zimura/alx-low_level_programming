5c4eef6f30cf77431a4fb5a5e98ac17675f5f4ae
zimura <chidimmaibezim0@gmail.com>
2022-09-12 10:05:55 -0700

    two combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,11 +19,11 @@ int main(void)
 	{
 	if (j != i)
 	{
-		putchar((i / 10) + 48);
-		putchar((i % 10) + 48);
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 		putchar(' ');
-		putchar((j / 10) + 48);
-		putchar((j % 10) + 48);
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
 	if (i != 98 && j != 99)
 	{
 		putchar(',');
