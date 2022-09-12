30dc84ca274e1645b87a4bc667f75271e465f08f
zimura <chidimmaibezim0@gmail.com>
2022-09-12 00:46:46 -0700

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two digits
+* Description: Using main function
+* this program print "{0...9}"
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = i+1; j <= 9; j++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+	if (i == 9 && j == 9)
+	{
+		continue;
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* maint - prints all possible combinations 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+* Description: Using the main function
+* this program should  be possible to print "{0...9}" in all possible ways
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	if (c != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
