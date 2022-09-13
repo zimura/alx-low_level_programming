d73a0fce60ecfc3da11b3fc46653a348d312dbb2
zimura <chidimmaibezim0@gmail.com>
2022-09-13 15:12:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+* _putchar - writes the character c to stdout
+* @c: @c: The character to print
+* Return: 0
+*/
+
+int _putchar(char c)
+{
+	putchar("_putchar");
+	putchar('\n');
+	return (0);
+}
