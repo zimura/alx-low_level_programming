82a89d5c43fa4268ad31dd4759ccf0c5604a1667
zimura <chidimmaibezim0@gmail.com>
2022-09-13 23:46:44 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,8 +22,8 @@ void times_table(void)
 	if (c <= 9)
 		_putchar(' ');
 	else
-		_putchar((c / 10) + '0');
-		_putchar((c % 10) + '0');
+	_putchar((c / 10) + '0');
+	_putchar((c % 10) + '0');
 	}
 	_putchar('\n');
 	}
