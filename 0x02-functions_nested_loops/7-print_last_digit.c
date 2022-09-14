546639c7cfdbb320fb6fe8eb2e6817030d8df8ec
zimura <chidimmaibezim0@gmail.com>
2022-09-13 18:48:37 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints the last digit of a number
+* @n: the charcter that it prints
+* Description: using the print_last_digit function
+* this program should prints the last digit of a number
+* Return: Always 0
+*/
+int print_last_digit(int n)
+{
+	int c;
+
+	if (n < 0)
+	n = -n;
+	c = n % 10;
+	_putchar(c + '0');
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer
+* Description: using the jack_bauer function
+* this program should prints every minute of the day of Jack Bauer
+* Return: Always 0
+*/
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+	for (j = 0; j < 60; j++)
+	{
+	if (i < 10)
+	{
+		_putchar('0');
+		_putchar(i + '0');
+	}
+	else if (i >= 10)
+	{
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+	}
+	if (j < 10)
+	{
+		_putchar(':');
+		_putchar('0');
+		_putchar(j + '0');
+	}
+	else if (j >= 10)
+	{
+		_putchar(':');
+		_putchar((j / 10) + '0');
+		_putchar((j % 10) + '0');
+	}
+	_putchar('\n');
+	}
+	}
+}
