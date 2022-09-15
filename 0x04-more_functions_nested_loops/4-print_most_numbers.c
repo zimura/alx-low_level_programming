d128dec381fc596cb3d69e23c909b5cf785d9669
zimura <chidimmaibezim0@gmail.com>
2022-09-15 16:08:48 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 0; a <= 9 a++)
+	for (a = 0; a <= 9; a++)
 	{
 	if (a != 2 && a != 4)
 		_putchar(a + '0');
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,7 +17,7 @@ void more_numbers(void)
 	{
 	if (y >= 10)
 		_putchar((y / 10) + '0');
-		_putchar((y % 10) + '0');
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
