af0fce788399400e310426ea605def6df10e0997
zimura <chidimmaibezim0@gmail.com>
2022-09-15 15:20:48 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,10 +4,9 @@
 * _isdigit - checks for a digit (0 through 9)
 * @c: that prints the charcter
 * Description: using the _isdigit function
-* the program should check for digits
+* the program should check for digit
 * Return: Success
 */
-
 int _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.">
+
+/**
+* print_numbers -  prints the numbers
+* from 0 to 9, followed by a new line
+* Description: Using the print_numbers function
+* this program should print 0 to 9
+* Return: success
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	_putchar(a + '0');
+	_putchar('\n');
+}
