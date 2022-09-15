296f148982c068b4e1a166b4e8f3555441da2392
zimura <chidimmaibezim0@gmail.com>
2022-09-15 16:35:53 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,11 @@ void print_line(int n)
 	int x;
 
 	if (n <= 0)
-	_putchar('\n');
+		_putchar('\n');
 	else
 	{
 	for (x = 0; x < n; x++)
-		_putchar(95);
-		_putchar('\n');
+	_putchar(95);
+	_putchar('\n');
 	}
 }
