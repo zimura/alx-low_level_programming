2502c7d6c019087ea4e3514c22528a421c37e157
zimura <chidimmaibezim0@gmail.com>
2022-09-16 06:54:55 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -17,10 +18,10 @@ void more_numbers(void)
 	{
 	if (y > 9)
 	{
-	_putchar((y / 10) + '0');
+		_putchar((y / 10) + '0');
 	}
-	_putchar((y % 10) + '0');
+		_putchar((y % 10) + '0');
 	}
-	_putchar(10);
+		_putchar(10);
 	}
 }
