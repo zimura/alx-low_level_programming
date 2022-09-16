f7143afcf3b74071d02fbd8bb3d7e2540dc22be3
zimura <chidimmaibezim0@gmail.com>
2022-09-15 17:19:57 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,20 +11,16 @@ void print_diagonal(int n)
 {
 	int x, y;
 
-	if (n > 0)
+	if (n <= 0)
+	_putchar('\n');
+	else
 	{
 	for (x = 0; x < n; x++)
 	{
 	for (y = 0; y < x; y++)
-	{
 		_putchar(' ');
-	}
-	_putchar('\');
-	if (x == (n - 1))
-	{
-		continue;
+		_putchar(92);
+		_putchar('\n');
 	}
 	}
-	}
-	_putchar('\n');
 }
