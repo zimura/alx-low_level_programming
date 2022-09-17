cac503420cb6a8ddb0e37700dc55a60098f331c0
zimura <chidimmaibezim0@gmail.com>
2022-09-16 19:01:53 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,41 +1,9 @@
-#include <stdlib.h>
-
-#include <time.h>
-
-#include <stdio.h>
-
-
-void positive_or_negative(int i)
-{
-
-	int n;
-
-
-
-		srand(time(0));
-
-			n = rand() - RAND_MAX / 2;
-
-			if (n > 0)
-			{
-				printf("%i is positive\n", n);
-			}
-			else if (n == 0)
-			{
-				printf("%i is zero\n", n);
-			}
-			else if (n < 0)
-			{
-				printf("%i is zero\n", n);
-			}
-			return (0);
-}
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
