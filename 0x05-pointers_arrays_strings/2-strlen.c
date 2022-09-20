bc4b37335b1efc73715a4710c4250f2dd6d9a1d1
zimura <chidimmaibezim0@gmail.com>
2022-09-19 19:21:56 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,14 +6,7 @@
 * @s:the pointer to pass the function
 * Return: length of a string
 */
-
 int _strlen(char *s)
 {
-	int len;
-
-	for (; *s != '\0'; s++)
-	{
-		len += 1;
-	}
 	return (strlen(s));
 }
