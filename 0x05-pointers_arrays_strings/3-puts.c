e4bc1ca440d95c8ce94663fdc6010d88590792dd
zimura <chidimmaibezim0@gmail.com>
2022-09-19 19:33:27 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _puts -  print string followed be a new line
+* @str: accepts parameter
+*/
+void _puts(char *str)
+{
+	for (; str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
