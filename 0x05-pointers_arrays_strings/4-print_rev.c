834429b042676ff0589876aeaff498090dff2bdf
zimura <chidimmaibezim0@gmail.com>
2022-09-19 20:18:38 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,19 @@
 #include "main.h"
+#include <string.h>
+/**
+* print_rev - prints a string in reverse order
+* @s: the print paramter
+*/
+void print_rev(char *s)
+{
+	int i, n;
 
-
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
