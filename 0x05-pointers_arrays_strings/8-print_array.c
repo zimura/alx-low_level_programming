418f343c59efa324e82d37aa9201a5e02a36034e
zimura <chidimmaibezim0@gmail.com>
2022-09-19 22:02:04 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_array - prints n digit of array
+* @a: array
+* @n: number of values
+* Return: a and n iuputs
+*/
+void print_array(int *a, int n)
+{
+	int s;
+
+	for (s = 0; s < n; s++)
+	{
+		printf("%d", a[s]);
+		if (s != n - 1)
+			print (", ");
+	}
+	printf('\n');
+}
