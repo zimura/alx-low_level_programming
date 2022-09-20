b173f7340c671f6395862715656c658f117f2488
zimura <chidimmaibezim0@gmail.com>
2022-09-19 21:16:14 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* puts2 - prints every other character of a string
+* @str: accepts string paraameter
+*/
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
