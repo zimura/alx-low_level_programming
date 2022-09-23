a51783c655e2e3787289b7082a4e1680bf14ee64
zimura <chidimmaibezim0@gmail.com>
2022-09-22 23:47:59 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -44,11 +44,11 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", i * 10);
 	if (i < size / 10)
 	{
-	print_line(b, 9, i);
+		print_line(b, 9, i);
 	}
 	else
 	{
-	print_line(b, size % 10 - 1, i);
+		print_line(b, size % 10 - 1, i);
 	}
 	putchar('\n');
 	}
