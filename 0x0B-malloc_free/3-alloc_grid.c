00c2aa0eb0d37da16d03f1798f12579cbdad0f4a
zimura <chidimmaibezim0@gmail.com>
2022-10-03 22:51:25 -0700

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 	if (grid[i] == NULL)
 	{
-	for (i = i - 1; i >= 0; i++)
+	for (i = i - 1; i >= 0; i--)
 	{
 		free(grid[i]);
 	}
