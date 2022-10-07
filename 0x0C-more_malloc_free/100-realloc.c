b5d072b2e053ec228b0418d1363b767e62382f48
zimura <chidimmaibezim0@gmail.com>
2022-10-07 03:04:54 -0700

    hot fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	return (0);
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(relocate + i) == nclone[i];
+		*(relocate + i) = nclone[i];
 	}
 	free(ptr);
 	return (relocate);
