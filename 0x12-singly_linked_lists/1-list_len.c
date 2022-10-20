55f859245c3cba153ba50c6e5b7c6a70b2f31601
zimura <chidimmaibezim0@gmail.com>
2022-10-20 07:09:59 -0700

    add node at the end

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - finds the number of element in list_t list
+ * @h: the list_t list
+ * Return: the number of element in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
